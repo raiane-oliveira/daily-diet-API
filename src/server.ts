4e995a2ca9b6c5165aa3cf583ec3ff7585ac99ce
Raiane <raiane.oliveira404@gmail.com>
2023-11-24 16:00:10 -0300

    chore: configure to deploy

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { app } from './app'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log('HTTP Server Running')
